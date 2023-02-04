992e6aca5a72e7e210c8c7c3f266bdd6dafe1086
Viltskaa <andrewstyou@gmail.com>
2023-02-04 21:05:48 +0400

    Init SpringWeb Application

---
+++ src/main/java/com/example/ip_labs/IpLabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ip_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLabsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IpLabsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/ip_labs/IpLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ip_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLabsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
